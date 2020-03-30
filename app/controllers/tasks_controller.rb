96558f54941d4cf5bc3163b3f49ad16cbb35da1c
naoya kogure <naolock@mbp.nifty.com>
2020-03-30 11:37:04 +0000

    for heroku_re

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
   def index
-    @task = current_user.tasks.build  # form_with 用
     @tasks = current_user.tasks.order(id: :desc).page(params[:page])
   end
   
